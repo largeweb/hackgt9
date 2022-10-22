cf6affa0dedf518c81655a329b7f530c0ad159c9
matt <mat@unc.edu>
2022-10-22 02:27:43 -0400

    test

--- find-my-food/src/index.js
+++ find-my-food/src/index.js
@@ -5,14 +5,23 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import SecondPage from './pages/SecondPage';
+import SuperCoolPage from './pages/SuperCoolPage';
+
+import fs from 'fs'
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App/>} />
-        <Route path="/page2" element={<Exercise/>} />
-        <Route path="/cool-page" element={<PlayPage/>} />
+        <Route path="/page2" element={<SecondPage/>} />
+        <Route path="/cool-page" element={<SuperCoolPage/>} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
