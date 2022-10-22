6b127417151125f91b245aa7c38fe1703fab3b0e
matt <mat@unc.edu>
2022-10-22 02:47:29 -0400

    small changes

--- find-my-food/src/pages/SecondPage.js
+++ find-my-food/src/pages/SecondPage.js
@@ -7,14 +7,14 @@ import useLocalStorage from "react-use-localstorage"
 
 function SecondPage() {
 
-  const [userInput, setUserInput] = useLocalStorage('userInput', 'empty')
+//   const [userInput, setUserInput] = useLocalStorage('userInput', 'empty')
 
   return (
     <div className="App">
       <div className='mainmenu'>
         <h1>⭕ SECOND PAGE 🤟 ⭕</h1>
         <Link to={'/'}><button className='menubutton' >Back Home</button></Link>
-        <div>{userInput}</div>
+        {/* <div>{userInput}</div> */}
         <Link to={'/cool-page'}><button className='menubutton' >SUPER COOL PAGE 📪</button></Link>
       </div>
     </div>
--- find-my-food/src/pages/SuperCoolPage.js
+++ find-my-food/src/pages/SuperCoolPage.js
@@ -7,14 +7,14 @@ import useLocalStorage from "react-use-localstorage"
 
 function SuperCoolPage() {
 
-  const [userInput, setUserInput] = useLocalStorage('userInput', 'empty')
+//   const [userInput, setUserInput] = useLocalStorage('userInput', 'empty')
 
   return (
     <div className="App">
       <div className='mainmenu'>
         <h1>⭕ SUPER COOL PAGE 🤟 ⭕</h1>
         <Link to={'/'}><button className='menubutton' >Back To App (/)</button></Link>
-        <div>{userInput}</div>
+        {/* <div>{userInput}</div> */}
         <Link to={'/page2'}><button className='menubutton' >page 2 ?</button></Link>
       </div>
     </div>
