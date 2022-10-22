32cb0f8a2c2ade0542ef8bfc5daf0db3133f7c7a
matt <mat@unc.edu>
2022-10-22 16:59:48 -0400

    cleaned up unused directories etc

--- fmf/src/index.js
+++ fmf/src/index.js
@@ -3,17 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import App from './App';
-import AppAdmin from './AppAdmin';
 import Login from './Login';
-import CubiclePage from './pages/CubiclePage';
-import CubicleManager from './pages/CubicleManager';
-import ChildCarePage from './pages/ChildCarePage';
-import ChildCareManagerPage from './pages/ChildCareManagerPage';
-import SocialHolidaysPage from './pages/SocialHolidaysPage';
-import SocialHolidaysManagerPage from './pages/SocialHolidaysManagerPage';
-import RDPRequestPage from './pages/RDPRequestPage';
-import RDPManagerPage from './pages/RDPManagerPage';
-import AboutPage from './pages/AboutPage';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 // import useLocalStorage from "./hooks/useLocalStorage"
@@ -25,8 +15,6 @@ import HomeDashboardPage from './pages/HomeDashboardPage';
 import PreferencePage from './pages/PreferencePage';
 import DirectionPage from './pages/DirectionPage';
 import RatingPage from './pages/RatingPage';
-
-// import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -46,25 +34,11 @@ root.render(
         <Route path="/" element={<IntroHomePage/>} />
         <Route path="/login" element={<LoginPage/>} />
         <Route path="/dashboard" element={<HomeDashboardPage/>} />
-        {/* <Route path="/filter" element={<FilterPage/>} /> */}
         <Route path="/preferences" element={<PreferencePage/>} />
         <Route path="/directions" element={<DirectionPage/>} />
         <Route path="/rating" element={<RatingPage/>} />
-        {/* <Route path="/" element={<Login/>} />
-        <Route path="/app" element={<App/>} />
-        <Route path="/appadmin" element={<AppAdmin/>} />
-        <Route path="/cubicle" element={<CubiclePage/>} />
-        <Route path="/cubiclemanager" element={<CubicleManager/>} />
-        <Route path="/childcare" element={<ChildCarePage />} />
-        <Route path="/childcaremanager" element={<ChildCareManagerPage />} />
-        <Route path="/social" element={<SocialHolidaysPage />} />
-        <Route path="/socialmanager" element={<SocialHolidaysManagerPage />} />
-        <Route path="/rdp" element={<RDPRequestPage />} />
-        <Route path="/rdpmanager" element={<RDPManagerPage />} />
-        <Route path="/about" element={<AboutPage />} /> */}
       </Routes>
     </BrowserRouter>
-    {/* <App /> */}
   </React.StrictMode>
 );
 
