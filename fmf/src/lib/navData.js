52be05664665aba6ee4a2bc3387f9ea7b2a2a051
matt <mat@unc.edu>
2022-10-22 18:33:11 -0400

    small navbar bug

--- fmf/src/lib/navData.js
+++ fmf/src/lib/navData.js
@@ -3,29 +3,41 @@ import TravelExploreIcon from '@mui/icons-material/TravelExplore';
 import BarChartIcon from '@mui/icons-material/BarChart';
 import SettingsIcon from '@mui/icons-material/Settings';
 
+// profile
+// past history
+// preferences
+// dashboard
+// login/logout
+
 export const navData = [
     {
         id: 0,
         icon: <HomeIcon/>,
-        text: "Home",
+        text: "Profile",
         link: "/"
     },
     {
         id: 1,
         icon: <TravelExploreIcon/>,
-        text: "Explore",
-        link: "explore"
+        text: "History",
+        link: "/"
     },
     {
         id: 2,
         icon: <BarChartIcon/>,
-        text: "Statistics",
-        link: "statistics"
+        text: "Preferences",
+        link: "/preferences"
     },
     {
         id: 3,
         icon: <SettingsIcon/>,
-        text: "Settings",
-        link: "settings"
+        text: "Dashboard",
+        link: "/dashboard"
+    },
+    {
+        id: 4,
+        icon: <SettingsIcon/>,
+        text: "Logout",
+        link: "/"
     }
 ]
