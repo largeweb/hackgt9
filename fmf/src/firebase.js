8fc772d318829fb3d24eacdb373c1ea4e5c6088f
matt <mat@unc.edu>
2022-10-23 05:12:47 -0400

    npm i

--- fmf/src/firebase.js
+++ fmf/src/firebase.js
@@ -1,10 +1,16 @@
 // Import the functions you need from the SDKs you need
 // import dotenv from 'dotenv'
 // dotenv.config()
-import * as firebase from "firebase";
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import "firebase/auth";
+// import * as firebase from "firebase/app";
+import { initializeApp } from 'firebase/app';
+// import { getAnalytics } from "firebase/analytics";
+// import "firebase/auth";
+
+import { getAuth, signInWithPopup, signInWithRedirect, getRedirectResult, GoogleAuthProvider, createUserWithEmailAndPassword } from "firebase/auth";
+
+const provider = new GoogleAuthProvider();
+// provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 // Your web app's Firebase configuration
@@ -19,15 +25,62 @@ const firebaseConfig = {
   measurementId: "G-KSBB0YHWTQ"
 };
 // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+auth.languageCode = 'it';
+
+signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+
+  signInWithRedirect(auth, provider);
+
+  getRedirectResult(auth)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access Google APIs.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+
+    // The signed-in user info.
+    const user = result.user;
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+
+// To apply the default browser preference instead of explicitly setting it.
+// firebase.auth().useDeviceLanguage();
+
 // const analytics = getAnalytics(app);
 
-export const auth = firebase.auth();
-const googleProvider = new firebase.auth.GoogleAuthProvider()
-export const signInWithGoogle = () => {
-  auth.signInWithPopup(googleProvider).then((res) => {
-    console.log(res.user)
-  }).catch((error) => {
-    console.log(error.message)
-  })
-}
+// export const auth = firebase.auth();
+// const googleProvider = new firebase.auth.GoogleAuthProvider()
+// export const signInWithGoogle = () => {
+//   auth.signInWithPopup(googleProvider).then((res) => {
+//     console.log(res.user)
+//   }).catch((error) => {
+//     console.log(error.message)
+//   })
+// }
