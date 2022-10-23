390c76999455b55c9cc14e034069a06761133cfc
matt <mat@unc.edu>
2022-10-23 04:43:14 -0400

    npm i

--- fmf/src/firebase.js
+++ fmf/src/firebase.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import dotenv from 'dotenv'
+// import dotenv from 'dotenv'
 dotenv.config()
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
