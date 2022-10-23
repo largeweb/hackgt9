9175fc3d7e4ee2397655aff92fcfcf1708938b2e
Ernest Ermongkonchai <erneste@email.unc.edu>
2022-10-23 08:20:33 -0400

    pref

--- fmf/src/pages/PreferencePage.js
+++ fmf/src/pages/PreferencePage.js
@@ -19,6 +19,7 @@ function PreferencePage() {
     <div className='midTitleText centeronly' style={{color:"#5838b4"}}>
       Preference
     </div>
+    <hr></hr>
     <h1 className='smallTitleText' style={{marginLeft:"5%"}}>Category</h1>
     <div class="grid-container">
       <div class="grid-item">
@@ -46,33 +47,36 @@ function PreferencePage() {
         </div>
       </div>
     </div> 
+    <br></br>
+    <hr></hr>
     <h1 className='smallTitleText' style={{marginLeft:"5%"}}>Restrictions</h1>
     <div class="grid-container">
       <div class="grid-item">
         <div className="picture-frame">
           <img src='https://tipbuzz.com/wp-content/uploads/Grilled-Steak-11-500x500.jpg' alt='steak' />
-          <span>Texan</span>
+          <span>Paleo</span>
         </div>
       </div>
       <div class="grid-item">
         <div className="picture-frame">
           <img src='https://www.karissasvegankitchen.com/wp-content/uploads/2020/06/vegan-carrot-dogs-ft-500x500.jpg' alt='vegan' />
-          <span>American</span>
+          <span>HotDog</span>
         </div>
       </div>
       <div class="grid-item">
         <div className="picture-frame">
           <img src='https://www.funfoodfrolic.com/wp-content/uploads/2020/03/Tofu-Stir-Fry-Thumbnail-500x500.jpg' alt='stirfry' />
-          <span>Chinese</span>
+          <span>Vegetarian</span>
         </div>
       </div>
       <div class="grid-item">
         <div className="picture-frame">
           <img src='https://sharpaspirant.com/wp-content/uploads/2021/02/Breakfast-Sandwich-22-500x500.jpg' alt='sandwich' />
-          <span>European</span>
+          <span>Kosher</span>
         </div>
       </div>
     </div> 
+    <hr></hr>
     <br></br>
 	  <Link to="/directions"><button className='button-google'>Submit</button></Link>
     <br></br>
