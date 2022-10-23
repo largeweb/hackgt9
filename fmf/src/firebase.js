b6a455ba9337d9cb7d2231a8014570ddc231365d
matt <mat@unc.edu>
2022-10-23 05:14:34 -0400

    npm i

--- fmf/src/firebase.js
+++ fmf/src/firebase.js
@@ -27,7 +27,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
-export const auth = getAuth(app);
+const auth = getAuth(app);
+export default auth;
 auth.languageCode = 'it';
 
 signInWithPopup(auth, provider)
