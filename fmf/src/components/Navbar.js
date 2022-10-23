7a7eb3dab46f968fde7b91fc074bf047a196c823
matt <mat@unc.edu>
2022-10-23 07:15:50 -0400

    worked on icon:

--- fmf/src/components/Navbar.js
+++ fmf/src/components/Navbar.js
@@ -27,7 +27,7 @@ const Navbar = () => {
                 {/* <KeyboardDoubleArrowLeftIcon /> */}
                 {/* <i class="fa fa-bars" aria-hidden="true"></i> */}
                 {/* <hamburgerIcon style={{backgroundColor:"orange", padding:"10px"}} /> */}
-                <img src={hamburgerIcon} style={{backgroundColor:"gray", width:"50px", height:"50px"}} />
+                <img src={hamburgerIcon} style={{backgroundColor:"gray", width:"40px", height:"40px"}} />
               </button>
               {navData.map(item =>{
                 return <NavLink key={item.id} className='navItem' to={item.link}>
@@ -44,7 +44,7 @@ const Navbar = () => {
                 {/* <i class="fa fa-bars" aria-hidden="true"></i> */}
                 {/* <KeyboardDoubleArrowLeftIcon /> */}
                 {/* <hamburgerIcon style={{backgroundColor:"orange",padding:"10px"}} /> */}
-                <img src={hamburgerIcon} style={{backgroundColor:"gray", width:"50px", height:"50px"}} />
+                <img src={hamburgerIcon} style={{backgroundColor:"gray", width:"40px", height:"40px"}} />
               </button>
               {/* {navData.map(item =>{
                 return <NavLink key={item.id} className='navItem' to={item.link}>
