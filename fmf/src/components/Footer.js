92f7f5258b48e6f4397e702e04cd6d034667a9ef
matt <mat@unc.edu>
2022-10-23 02:11:32 -0400

    direction page change

--- fmf/src/components/Footer.js
+++ fmf/src/components/Footer.js
@@ -32,51 +32,51 @@ export default function Footer() {
         </div>
       </section> */}
 
-      <section className='footertype'>
-        <div className='container text-center text-md-start mt-5'>
-          <div className='row mt-3'>
-            <div className='col-md-3 col-lg-4 col-xl-3 mx-auto mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>
-                <i className='fas fa-gem me-3'></i>Find my Food
-              </h6>
-              <div>
-                Don't know what to eat?<br></br>Find My Food is here to help you find the best food in your area.
-              </div>
-            </div>
-
-            {/* <div className='col-md-2 col-lg-2 col-xl-2 mx-auto mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Actions</h6>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Button1
-                </a>
-              </p>
-            </div> */}
-
-            {/* <div className='col-md-3 col-lg-2 col-xl-2 mx-auto mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Useful links</h6>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Link 1
-                </a>
-              </p>
-            </div> */}
-
-            {/* <div className='col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Contact</h6>
-              <p>
-                <i className='fas fa-home me-3'></i>Address
-              </p>
-              <p>
-                <i className='fas fa-envelope me-3'></i>example@infosys.com
-              </p>
-              <p>
-                <i className='fas fa-print me-3'></i> Phone # 2
-              </p>
-            </div> */}
-          </div>
-        </div>
-      </section>
+      {/* <section className='footertype'> */}
+        {/* <div className='container text-center text-md-start mt-5'> */}
+          {/* <div className='row mt-3'> */}
+            {/* <div className='col-md-3 col-lg-4 col-xl-3 mx-auto mb-4'> */}
+              {/* <h6 className='text-uppercase fw-bold mb-4'> */}
+                {/* <i className='fas fa-gem me-3'></i>Find my Food */}
+              {/* </h6> */}
+              {/* <div> */}
+                {/* Don't know what to eat?<br></br>Find My Food is here to help you find the best food in your area. */}
+              {/* </div> */}
+            {/* </div> */}
+{/*  */}
+            {/* {/* <div className='col-md-2 col-lg-2 col-xl-2 mx-auto mb-4'> */}
+              // <h6 className='text-uppercase fw-bold mb-4'>Actions</h6>
+              // <p>
+                // <a href='#!' className='text-reset'>
+                  // Button1
+                // </a>
+              // </p>
+            // </div> */}
+{/*  */}
+            {/* {/* <div className='col-md-3 col-lg-2 col-xl-2 mx-auto mb-4'> */}
+              // <h6 className='text-uppercase fw-bold mb-4'>Useful links</h6>
+              // <p>
+                // <a href='#!' className='text-reset'>
+                  // Link 1
+                // </a>
+              // </p>
+            // </div> */}
+{/*  */}
+            {/* {/* <div className='col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4'> */}
+              // <h6 className='text-uppercase fw-bold mb-4'>Contact</h6>
+              // <p>
+                // <i className='fas fa-home me-3'></i>Address
+              // </p>
+              // <p>
+                // <i className='fas fa-envelope me-3'></i>example@infosys.com
+              // </p>
+              // <p>
+                // <i className='fas fa-print me-3'></i> Phone # 2
+              // </p>
+            // </div> */}
+          {/* </div> */}
+        {/* </div> */}
+      {/* </section> */}
 
       <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
         © 2022 Copyright:<br></br>
